ab54d4c932f6e06b47c0897f639e050f5e66d94d
kcandeloni <kcandeloni@gmail.com>
2022-09-12 06:01:59 -0300

    Modificação das rotas

--- src/routes/private.routers.js
+++ src/routes/private.routers.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import  verificaToken from '../middlewares/tokenValidation.middleware.js';
 import my_data from '../controllers/my_data.controllers.js';
-import update_wallet from '../controllers/new_receive.controllers.js';
+import update_wallet from '../controllers/update_wallet.controllers.js';
 import validaWallet from '../middlewares/walletValidation.middleware.js';
 
 const authRouter = express.Router();
-authRouter.get('/my-data', verificaToken, my_data);
-authRouter.post('/new-receive',
+authRouter.get('/mywallet', verificaToken, my_data);
+authRouter.post('/mywallet',
     verificaToken,
     validaWallet,
     update_wallet);
